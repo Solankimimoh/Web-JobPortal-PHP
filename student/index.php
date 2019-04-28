3352e629abb1a86f331b81bbfc77b5a763c75159
solankimimoh <solankimimoh@gmail.com>
2019-04-29 01:05:08 +0530

    images add and map path change

--- student/index.php
+++ student/index.php
@@ -1,137 +1,161 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>Freelancer - Start Bootstrap Theme</title>
+    <title>Freelancer - Start Bootstrap Theme</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
-  <!-- Plugin CSS -->
-  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
 
-  <!-- Custom styles for this template -->
-  <link href="css/freelancer.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/freelancer.min.css" rel="stylesheet">
 
 </head>
 
 <body id="page-top">
 
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Job Portal</a>
-      <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Home</a>
-          </li>
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a>
-          </li>
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact US</a>
-          </li>
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="login.php">Getting Started</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <!-- Header -->
-  <header class="masthead bg-primary text-white text-center" id="portfolio">
-    <div class="container">
-      <img class="img-fluid mb-5 d-block mx-auto" src="img/profile.png" alt="">
-      <h1 class="text-uppercase mb-0">Job Portal</h1>
-      <hr class="star-light">
-      <h2 class="font-weight-light mb-0">Student - Job Portal</h2>
-    </div>
-  </header>
-
- 
-  <!-- About Section -->
-  <section class=" text-dark mb-0" id="about">
-    <div class="container">
-      <h2 class="text-center text-uppercase text-dark">About US</h2>
-      <hr class="star-light mb-5">
-      <div class="row">
-        <div class="col-lg-12 ml-auto">
-          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur porro quibusdam rem dolores debitis accusantium sint, deleniti dicta, atque, placeat beatae. Iure et, reprehenderit architecto eum velit ullam corporis facere modi, dolorum, deleniti officiis illum consequatur. Velit, commodi, sit omnis, iusto nesciunt, doloribus asperiores ut totam quaerat fugit placeat ab saepe excepturi aliquam. Assumenda necessitatibus vel consequuntur, nostrum culpa officia quidem vitae numquam repellat odio voluptatibus ratione sequi, delectus sapiente, qui dolorem asperiores, non dicta. Dignissimos tenetur ratione officia unde tempore ipsam autem similique. Nisi assumenda, aliquid, aspernatur laborum sint fugiat ipsam ea provident reiciendis. Fuga non sapiente suscipit, quidem error ratione veniam sed debitis, ab molestiae minus iusto eaque asperiores officiis nisi, voluptates beatae repudiandae. Vero odio dolorem dicta explicabo alias, nisi quasi! Quam delectus tempore, excepturi necessitatibus maxime fugit perferendis alias asperiores totam culpa, ipsum odio, aliquam molestiae quisquam error mollitia commodi officia illo laudantium molestias eveniet distinctio.</p>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">Job Portal</a>
+            <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Home</a>
+                    </li>
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a>
+                    </li>
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#gallery">Gallery</a>
+                    </li>
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact US</a>
+                    </li>
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="login.php">Getting Started</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-     
-      </div>
-      <div class="text-center mt-4">
-        <a class="btn btn-xl btn-outline-light" href="#">
-          <i class="fas fa-download mr-2"></i>
-          Download Now!
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead bg-primary text-white text-center" id="portfolio">
+        <div class="container">
+            <img class="img-fluid w-50 mb-5 d-block mx-auto" src="https://www.dentistfriend.com//uploads/praxisimages/Find-a-Job.png" alt="">
+            <h1 class="text-uppercase mb-0">Job Portal</h1>
+            <hr class="star-light">
+            <h2 class="font-weight-light mb-0">Student - Job Portal</h2>
+        </div>
+    </header>
+
+
+    <!-- About Section -->
+    <section class=" text-dark mb-0" id="about">
+        <div class="container">
+            <h2 class="text-center text-uppercase text-dark">About US</h2>
+            <hr class="star-light mb-5">
+            <div class="row">
+                <div class="col-lg-12 ml-auto">
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur porro quibusdam rem dolores debitis accusantium sint, deleniti dicta, atque, placeat beatae. Iure et, reprehenderit architecto eum velit ullam corporis facere modi, dolorum, deleniti officiis illum consequatur. Velit, commodi, sit omnis, iusto nesciunt, doloribus asperiores ut totam quaerat fugit placeat ab saepe excepturi aliquam. Assumenda necessitatibus vel consequuntur, nostrum culpa officia quidem vitae numquam repellat odio voluptatibus ratione sequi, delectus sapiente, qui dolorem asperiores, non dicta. Dignissimos tenetur ratione officia unde tempore ipsam autem similique. Nisi assumenda, aliquid, aspernatur laborum sint fugiat ipsam ea provident reiciendis. Fuga non sapiente suscipit, quidem error ratione veniam sed debitis, ab molestiae minus iusto eaque asperiores officiis nisi, voluptates beatae repudiandae. Vero odio dolorem dicta explicabo alias, nisi quasi! Quam delectus tempore, excepturi necessitatibus maxime fugit perferendis alias asperiores totam culpa, ipsum odio, aliquam molestiae quisquam error mollitia commodi officia illo laudantium molestias eveniet distinctio.</p>
+                </div>
+
+            </div>
+      
+        </div>
+    </section>
+
+
+    <section id="gallery">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <img class="w-100" src="https://static-news.moneycontrol.com/static-mcnews/2017/05/jobs_hiring_1280-770x433.jpg" />
+                </div>
+                <div class="col-4">
+                    <img class="w-100" src="https://www.rozee.pk/blog/wp-content/uploads/2017/10/job-vs-career-infographic-1068x607.jpg" />
+                </div>
+                <div class="col-4">
+                    <img class="w-100" src="http://clipart-library.com/img/1788430.jpg" />
+                </div>
+                <div class="col-4">
+                    <img class="w-100" src="https://blog.ipleaders.in/wp-content/uploads/2018/03/BV-Acharya-9.jpg" />
+                </div>
+                <div class="col-4">
+                    <img class="w-100" src="http://www.bhatkallys.com/wp-content/uploads/2015/08/jobs-1.jpg" />
+                </div>
+                <div class="col-4">
+                    <img class="w-100" src="https://fupping.com/wp-content/uploads/2018/06/Job-hunting.jpg" />
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- Contact Section -->
+    <section id="contact">
+        <div class="container-fluid">
+            <h2 class="text-center text-uppercase text-secondary mb-0">Contact US</h2>
+            <hr class="star-dark mb-5">
+            <div class="row">
+                <div class="col-lg-12 mx-auto">
+                    <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+                    <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14709.533967898618!2d72.7684251!3d22.8252972!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9411844c556039f!2sD+A+Diploma+Engineering+and+Technology!5e0!3m2!1sen!2sin!4v1556479986471!5m2!1sen!2sin" class="w-100" frameborder="0" style="border:0;height:550px" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <div class="copyright py-4 text-center text-white">
+        <div class="container">
+            <small>Copyright &copy; Your Website 2019</small>
+        </div>
+    </div>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-to-top  position-fixed ">
+        <a class="js-scroll-trigger d-block text-center text-white rounded bg-success" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
         </a>
-      </div>
     </div>
-  </section>
 
-  <!-- Contact Section -->
-  <section id="contact">
-    <div class="container-fluid">
-      <h2 class="text-center text-uppercase text-secondary mb-0">Contact US</h2>
-      <hr class="star-dark mb-5">
-      <div class="row">
-        <div class="col-lg-12 mx-auto">
-          <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
-          <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3670.3517506132216!2d72.52996031447071!3d23.08421598492191!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e83377e5ebb59%3A0x46fd2b30452c38a0!2sR.C.+Technical!5e0!3m2!1sen!2sin!4v1555397385994!5m2!1sen!2sin" class="w-100" frameborder="0" style="border:0;height:550px" allowfullscreen></iframe>
-        </div>
-      </div>
-    </div>
-  </section>
 
- 
-  <div class="copyright py-4 text-center text-white">
-    <div class="container">
-      <small>Copyright &copy; Your Website 2019</small>
-    </div>
-  </div>
 
-  <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
-  <div class="scroll-to-top  position-fixed ">
-    <a class="js-scroll-trigger d-block text-center text-white rounded bg-success" href="#page-top">
-      <i class="fa fa-chevron-up"></i>
-    </a>
-  </div>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
- 
- 
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
-  <!-- Plugin JavaScript -->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
 
-  <!-- Contact Form JavaScript -->
-  <script src="js/jqBootstrapValidation.js"></script>
-  <script src="js/contact_me.js"></script>
-
-  <!-- Custom scripts for this template -->
-  <script src="js/freelancer.min.js"></script>
+    <!-- Custom scripts for this template -->
+    <script src="js/freelancer.min.js"></script>
 
 </body>
 
